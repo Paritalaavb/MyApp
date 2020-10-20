657316f51dc9a8d9087e537eff199560170d46b7
Aravindbabu Paritala <72963678+Paritalaavb@users.noreply.github.com>
2020-10-20 12:53:56 +0530

    Add files via upload

---
+++ Test3/Test3/Controllers/OrderDetailsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Test3.Models;
+
+namespace Test3.Controllers
+{
+    public class OrderDetailsController : ApiController
+    {
+        private masterEntities db = new masterEntities();
+
+
+        // POST: api/OrderDetails
+        [ResponseType(typeof(tbl_OrderDetails))]
+        public IHttpActionResult Posttbl_OrderDetails(tbl_OrderDetails tbl_OrderDetails)
+        {
+
+            if (db.tbl_Products.Where(P => P.ProductId == tbl_OrderDetails.ProductId && P.Units == tbl_OrderDetails.Units).Count() == 0)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "this item does not exist"));
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var Products = db.tbl_Products.Where(p => p.ProductId == tbl_OrderDetails.ProductId).First();
+            Products.Units = Products.Units - tbl_OrderDetails.Units;
+
+            db.tbl_OrderDetails.Add(tbl_OrderDetails);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (tbl_OrderDetailsExists(tbl_OrderDetails.ProductId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tbl_OrderDetails.ProductId }, tbl_OrderDetails);
+        }
+
+
+        // GET: api/OrderDetails
+        public IQueryable<tbl_OrderDetails> Gettbl_OrderDetails()
+        {
+            return db.tbl_OrderDetails;
+        }
+
+        // GET: api/OrderDetails/5
+        [ResponseType(typeof(tbl_OrderDetails))]
+        public IHttpActionResult Gettbl_OrderDetails(int id)
+        {
+            tbl_OrderDetails tbl_OrderDetails = db.tbl_OrderDetails.Find(id);
+            if (tbl_OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tbl_OrderDetails);
+        }
+
+        // PUT: api/OrderDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttbl_OrderDetails(int id, tbl_OrderDetails tbl_OrderDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tbl_OrderDetails.ProductId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tbl_OrderDetails).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tbl_OrderDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+       
+
+        // DELETE: api/OrderDetails/5
+        [ResponseType(typeof(tbl_OrderDetails))]
+        public IHttpActionResult Deletetbl_OrderDetails(int id)
+        {
+            tbl_OrderDetails tbl_OrderDetails = db.tbl_OrderDetails.Find(id);
+            if (tbl_OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            db.tbl_OrderDetails.Remove(tbl_OrderDetails);
+            db.SaveChanges();
+
+            return Ok(tbl_OrderDetails);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tbl_OrderDetailsExists(int id)
+        {
+            return db.tbl_OrderDetails.Count(e => e.ProductId == id) > 0;
+        }
+    }
+}
---
+++ Test3/Test3/Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Test3.Models;
+
+namespace Test3.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private masterEntities db = new masterEntities();
+
+        // GET: api/Products
+        public IQueryable<tbl_Products> Gettbl_Products()
+        {
+            return db.tbl_Products;
+        }
+
+        // GET: api/Products/5
+        [ResponseType(typeof(tbl_Products))]
+        public IHttpActionResult Gettbl_Products(int id)
+        {
+            tbl_Products tbl_Products = db.tbl_Products.Find(id);
+            if (tbl_Products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tbl_Products);
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttbl_Products(int id, tbl_Products tbl_Products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tbl_Products.ProductId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tbl_Products).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tbl_ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(tbl_Products))]
+        public IHttpActionResult Posttbl_Products(tbl_Products tbl_Products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tbl_Products.Add(tbl_Products);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (tbl_ProductsExists(tbl_Products.ProductId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tbl_Products.ProductId }, tbl_Products);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(tbl_Products))]
+        public IHttpActionResult Deletetbl_Products(int id)
+        {
+            tbl_Products tbl_Products = db.tbl_Products.Find(id);
+            if (tbl_Products == null)
+            {
+                return NotFound();
+            }
+
+            db.tbl_Products.Remove(tbl_Products);
+            db.SaveChanges();
+
+            return Ok(tbl_Products);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tbl_ProductsExists(int id)
+        {
+            return db.tbl_Products.Count(e => e.ProductId == id) > 0;
+        }
+    }
+}
